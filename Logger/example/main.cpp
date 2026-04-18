376b675b7548d3b8e799a1d091330a1766f5c44b
Tristan Alderson <talderson42@gmail.com>
2026-04-17 20:28:32 -0400

    Add Logger library implementation with logging functionality

---
+++ Logger/example/main.cpp
@@ -0,0 +1,13 @@
+#include "logger.h"
+#include <SoftwareSerial.h>
+
+SoftwareSerial softSerial(10, 11);
+Logger logger(Serial, NODE_ID);       // HardwareSerial
+// Logger logger(softSerial, NODE_ID); // SoftwareSerial
+
+void setup()
+{
+    Serial.begin(115200);
+    g_logger = &logger;
+    LOG_INFO("Logger online");
+}
