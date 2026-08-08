74860602188273fe8565430cdfee8b95f49ced64
Tristan Alderson <talderson42@gmail.com>
2026-08-08 13:16:53 -0700

    chore(aim_network): bump library version to 0.7.1

--- aim_network/src/aim_network.h
+++ aim_network/src/aim_network.h
@@ -26,7 +26,7 @@
 
 namespace aim {
 
-static constexpr char kNetworkVersionString[] = "0.7.0";
+static constexpr char kNetworkVersionString[] = "0.7.1";
 static constexpr uint32_t kHeartbeatTxIntervalMs = 1000U;
 
 // --- CAN ID layout ---
